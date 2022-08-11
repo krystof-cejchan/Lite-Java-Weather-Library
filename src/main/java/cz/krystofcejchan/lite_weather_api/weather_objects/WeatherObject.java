983287943bede0a8ed1c806e789b7c58c1b03698
TheKrystof701 <krystofcejchan@seznam.cz>
2022-08-11 23:40:27 +0200

    quick fix for #2

--- src/main/java/cz/krystofcejchan/lite_weather_api/weather_objects/WeatherObject.java
+++ src/main/java/cz/krystofcejchan/lite_weather_api/weather_objects/WeatherObject.java
@@ -3,6 +3,10 @@ package cz.krystofcejchan.lite_weather_api.weather_objects;
 import cz.krystofcejchan.lite_weather_api.WeatherForeCast;
 import cz.krystofcejchan.lite_weather_api.enums_exception.enums.DAY;
 import cz.krystofcejchan.lite_weather_api.enums_exception.enums.TIME;
+import cz.krystofcejchan.lite_weather_api.weather_objects.subparts.current_weather.CurrentCondition;
+import cz.krystofcejchan.lite_weather_api.weather_objects.subparts.forecast.WeatherForecast;
+import cz.krystofcejchan.lite_weather_api.weather_objects.subparts.nearest_area.NearestArea;
+import cz.krystofcejchan.lite_weather_api.weather_objects.subparts.request.Request;
 import org.apache.commons.io.IOUtils;
 import org.jetbrains.annotations.NotNull;
 import org.json.JSONObject;
@@ -19,10 +23,10 @@ public abstract class WeatherObject<T> extends WeatherForeCast {
     private final JSONObject json;
 
 
-   /* private final CurrentCondition currentCondition;
+    private final CurrentCondition currentCondition;
     private final NearestArea nearestArea;
     private final Request request;
-    private final WeatherForecast weather;*/
+    private final WeatherForecast weather;
 
 
     public WeatherObject(String location, TIME[] times, DAY... days) throws IOException {
@@ -32,6 +36,11 @@ public abstract class WeatherObject<T> extends WeatherForeCast {
         this.location = super.getLocation();
         this.days = days;
         this.times = times;
+
+        this.currentCondition = new CurrentCondition(location);
+        this.nearestArea = new NearestArea(location);
+        this.request = new Request(location);
+        this.weather = new WeatherForecast(location, times, days);
     }
 
     public DAY[] getDays() {
@@ -53,6 +62,22 @@ public abstract class WeatherObject<T> extends WeatherForeCast {
 
     public JSONObject getJson() {
         return json;
+    }
+
+    public CurrentCondition getCurrentCondition() {
+        return currentCondition;
+    }
+
+    public NearestArea getNearestArea() {
+        return nearestArea;
+    }
+
+    public Request getRequest() {
+        return request;
+    }
+
+    public WeatherForecast getWeather() {
+        return weather;
     }
 
     @NotNull
