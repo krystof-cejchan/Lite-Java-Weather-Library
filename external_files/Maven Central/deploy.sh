6ffb1fd92c843931c80b278ff1367eba8d5fdd6f
TheKrystof701 <krystofcejchan@seznam.cz>
2022-08-15 19:30:26 +0200

    .sh updated

--- external_files/Maven Central/deploy.sh
+++ external_files/Maven Central/deploy.sh
@@ -1,1 +1,7 @@
+#!/bin/sh
+gpg --list-secret-keys --keyid-format LONG
+# https://maven.apache.org/plugins/maven-gpg-plugin/
+# https://maven.apache.org/plugins/maven-gpg-plugin/examples/deploy-signed-artifacts.html
+# https://maven.apache.org/plugins/maven-gpg-plugin/usage.html
 mvn clean deploy -Dgpg.skip -Darguments=-Dgpg.skip
+# mvn clean deploy
