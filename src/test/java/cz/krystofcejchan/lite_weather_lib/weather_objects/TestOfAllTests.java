99e8bd8035cedb224662eec95e1869e20289bc86
Kryštof Čejchan <krystofcejchan@seznam.cz>
2023-10-27 12:39:25 +0200

    v 1.1.4.1

--- src/test/java/cz/krystofcejchan/lite_weather_lib/weather_objects/TestOfAllTests.java
+++ src/test/java/cz/krystofcejchan/lite_weather_lib/weather_objects/TestOfAllTests.java
@@ -2,6 +2,8 @@ package cz.krystofcejchan.lite_weather_lib.weather_objects;
 
 import cz.krystofcejchan.lite_weather_lib.enums_exception.enums.DAY;
 import cz.krystofcejchan.lite_weather_lib.enums_exception.enums.TIME;
+import cz.krystofcejchan.lite_weather_lib.weather_objects.subparts.current_weather.CurrentCondition;
+import cz.krystofcejchan.lite_weather_lib.weather_objects.subparts.request.Request;
 
 import java.io.IOException;
 
@@ -10,7 +12,10 @@ public class TestOfAllTests {
     @org.junit.jupiter.api.Test
     void test() throws IOException {
         WeatherObject weatherObject = new WeatherObject("Mexico City", DAY.ALL, TIME.ALL);
-        //  Objects.requireNonNull(weatherObject.getWeatherForecast().getAllSavedForecasts()).forEach(System.out::println);
         weatherObject.getWeatherForecast().getForecastFor(DAY.TODAY,TIME.AM_6).print();
+        CurrentCondition currentCondition = new CurrentCondition("Prague");
+        currentCondition.print();
+        Request request = new Request("Moscow");
+        request.print();
     }
 }
